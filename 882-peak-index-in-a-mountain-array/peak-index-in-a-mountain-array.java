95385a65cf4adff541083d84589425eafa6a038b
Muthu Hariramar <166930626+muthuhariramar-dev@users.noreply.github.com>
2025-12-27 00:07:27 +0530

    Time: 0 ms (100.00%) | Memory: 80.6 MB (14.35%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,13 +1,25 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
+        //start
         int start =0;
+        //end
         int end =arr.length-1;
+        //binary Search loop :/
 
         while(start < end ){
+            //middle
             int mid = start + (end -  start)/2;
+
+            // if the current element is greater than next element your are in decresing Part of the array
             if(arr[mid]> arr[mid+1]){
+                //end is mid so no need to check beyond the decresing searching in left util loop voilate 
                 end = mid;
+
+            // your are in the Ascending part of the array
+            //so we increase start by 1
+
             }else{
+                //naturaly lay the highest number 
                 start = mid+1;
 
             }
